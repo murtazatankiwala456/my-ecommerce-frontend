598bfa5c4b18e9d73747417e49694907fdeb95c4
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-06-12 10:14:00 +0530

    completed till fetching products from dummy api (from folder-static)

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,13 +1,13 @@
 import NavBar from "../features/navbar/Navbar";
-import ProductDetail from "../features/product-list/components/ProductDetail";
+import ProductDetail from "../features/product/components/ProductDetail";
 function ProductDetailPage() {
-    return ( 
-        <div>
-            <NavBar>
-                <ProductDetail></ProductDetail>
-            </NavBar>
-        </div>
-     );
+  return (
+    <div>
+      <NavBar>
+        <ProductDetail></ProductDetail>
+      </NavBar>
+    </div>
+  );
 }
 
 export default ProductDetailPage;
