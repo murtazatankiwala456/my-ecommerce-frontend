0f7b7241c7070d82049876d85d06d19b53436e04
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-06-20 11:40:32 +0530

    product detail page added (dynamically)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
-} from 'react-router-dom';
-import Cart from './features/cart/Cart';
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
-import ProductDetailPage from './pages/ProductDetailPage';
+} from "react-router-dom";
+import Cart from "./features/cart/Cart";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import ProductDetailPage from "./pages/ProductDetailPage";
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home></Home>,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage></LoginPage>,
   },
   {
-    path: '/signup',
+    path: "/signup",
     element: <SignupPage></SignupPage>,
   },
-  { 
-    path: '/cart',
+  {
+    path: "/cart",
     element: <CartPage></CartPage>,
   },
-  { 
-    path: '/checkout',
+  {
+    path: "/checkout",
     element: <Checkout></Checkout>,
   },
-  { 
-    path: '/product-detail',
+  {
+    path: "/product-detail/:id", // :id provided by react-router
     element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -6,6 +6,14 @@ export function fetchAllProducts() {
     resolve({ data });
   });
 }
+export function fetchProductById(id) {
+  return new Promise(async (resolve) => {
+    // TODO: we will not hard coded server url here...
+    const response = await fetch("http://localhost:8080/products/" + id);
+    const data = await response.json();
+    resolve({ data });
+  });
+}
 export function fetchProductsByFilters(filter, sort, pagination) {
   // filter ={"brand":"Essence"}
   // TODO:we will on server  support mutilple value
